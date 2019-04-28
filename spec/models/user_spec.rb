45f9ff2de95a1b073aa15d983ad4815c00776e6c
raptorf1 <raptor_f1@hotmail.com>
2019-04-28 12:41:36 +0200

    Adds uniqueness validation test for the email to the Users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_presence_of :password } 
+    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_uniqueness_of :email }
   end
 end
